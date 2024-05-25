14fcd09b75263f57a3a091f2e48c39826e75a5aa
Zyrofx <144825148+Zyrofx@users.noreply.github.com>
2024-05-25 11:41:31 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,10 +8,10 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "20389440"))
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6932002690:AAFf3x9YzHDTdlK_EBMdBuSU8TXsFkI6gMY")
-    FSUB = getenv("FSUB", "VJ_Botz")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6573399998:AAHvZbhwRP9aNBPYrBWYEzumkI5mIgiXq6A")
+    FSUB = getenv("FSUB", "MvM_Links")
     CHID = int(getenv("CHID", "-1001926512163"))
-    SUDO = list(map(int, getenv("SUDO", "1746132193").split()))
+    SUDO = list(map(int, getenv("SUDO", "1746132193 1353275714").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rangnarl47:reqbotzyro@cluster0.zt0ld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
