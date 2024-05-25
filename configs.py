0d5d3f5fdff659497adf976dc4e9fba9d86b2136
Zyrofx <144825148+Zyrofx@users.noreply.github.com>
2024-05-25 13:40:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config:
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
     BOT_TOKEN = getenv("BOT_TOKEN", "6573399998:AAHvZbhwRP9aNBPYrBWYEzumkI5mIgiXq6A")
     FSUB = getenv("FSUB", "MvM_Links")
-    CHID = int(getenv("CHID", "-1001926512163"))
+    CHID = int(getenv("CHID", "-1002160712759"))
     SUDO = list(map(int, getenv("SUDO", "1746132193 1353275714").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rangnarl47:reqbotzyro@cluster0.zt0ld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     PORT = int(getenv("PORT", "8000"))
